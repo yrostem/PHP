2e81db071bf1d8e4299729f1bb21e39390e16edd
yrostem <yrostem@roc-dev.com>
2018-02-17 14:46:02 +0100

    busreis

--- busreis/index.php
+++ busreis/index.php
@@ -1,52 +1,22 @@
-<!--Ik heb alle opdrachten van "busreis" in 1 bestand gezegt, overzichtelijk zodat ik niet alles
-in apparte bestandjes hoef te zetten-->
-
-<html>
-<p>Kies de onderdeel om te bekijken</p>
-<form action="" method="POST">
-<input type="submit" name="autotyping" value="Autotyping">
-<input type="submit" name="bool" value="Boolean">
-<input type="submit" name="if" value="If-statement">
+<form method="POST" action="">
+	<input type="text" name="leeftijd" placeholder="Leeftijd"><br>
+	<input type="submit" name="submit" value="Check Prijs">
 </form>
-</html>
-
 <?php
-if(isset($_POST['autotyping'])){
+error_reporting(E_ALL);
 
-	#Onderdeel: Autotyping.
-	
-	$a = "Hello";
-	$b = "Wereld";
+$leeftijd = $_POST['leeftijd'];
 
-	$c = $a ." " . $b;
-
-	echo gettype($c). "<br>";
-
-	$d = 14;
-	$e = "1.4";
-	$f = $d + $e;
-	echo gettype($f);
-	echo "<br>" . $f;
-	
-	} elseif (isset($_POST['bool'])) {
-			
-			# Onderdeel boolean
-			$bool = 326; echo "De waarde is: " . $bool;
-			echo $bool ? "<br> Het is waar<br>" : "Het is niet waar<br>";
-			echo gettype($bool);
-
-	}elseif (isset($_POST['if'])) {
-		# Onderdeel if-statement
-		$bool = 326; echo "De waarde is: " . $bool;
-		$test = 324;
-		if ($bool > $test) {
-			
-			echo "Het is waar <br>";
-
-		} else {
-			echo "het is niet waar <br>";
-		}
+if(isset($_POST['submit'])){
+	if($leeftijd >= 13 && $leeftijd <= 65){
+		echo "Prijs is: 10€";
+	}elseif ($leeftijd >= 3 && $leeftijd <= 12 ) {
+		echo "Prijs is: 5€";
+	}elseif ($leeftijd > 65) {
+		echo "Prijs is 5€";
+	}elseif ($leeftijd < 3) {
+		echo "Je mag gratis mee";
 	}
 
-
+}
 ?>
